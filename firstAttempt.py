90d2dd7e8cb420b728ebbcd3f3fbfa2a463dc47f
LuisDiegoMora98 <72276020+LuisDiegoMora98@users.noreply.github.com>
2020-12-12 09:38:37 -0600

    Dates List within FechaOperacion SubElement
    Added getDatesList to create a list of range between one date and another, so we can fill movements and situations inside those XML nodes.
    
    PD: Consider client wants some days without actions, so we can either remove indexes from DatesList or other solution.

--- firstAttempt.py
+++ firstAttempt.py
@@ -2,21 +2,38 @@ from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 
 from xml.etree import ElementTree
 from xml.dom import minidom
+from datetime import datetime, timedelta
+from collections import OrderedDict
+
+def getDatesList(pIntervalListDate):
+    """Entry must be a list like this: ["2020-06-01", "2020-11-1"]
+                                        start ↑    ->   end ↑
+    The function returns a list with string dates of the range between the first position and second position on the entry list"""
+    start, end = [datetime.strptime(_, "%Y-%m-%d") for _ in pIntervalListDate]
+    datesList = list(OrderedDict(((start + timedelta(_)).strftime("%Y-%m-%d"), None) for _ in range((end - start).days)).keys())
+    return datesList
 
 def prettify(elem):
-    """Return a pretty-printed XML string for the Element.
-    """
+    #Return a pretty-printed XML string for the Element.
+    
     rough_string = ElementTree.tostring(elem, 'utf-8')
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
 
-top = Element('Operaciones')
+datesList = getDatesList(["2020-06-01", "2020-11-1"])
 
+top = Element('Operaciones')
+listaFechas = []
 children = [
-    Element('FechaOperacion', Fecha=str(i))
-    for i in range(3)
+    Element('FechaOperacion', Fecha=datesList[date])
+    for date in range(len(datesList))
     ]
 
 top.extend(children)
 
 print(prettify(top))
+
+
+
+
+
